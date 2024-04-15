7fdcad8f876295534fd2e620053d3112aefa19a9
Loïck Marin <marin.loick@gmail.com>
2024-04-15 08:59:18 +0000

    ajout effet visuel lampe torche

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,17 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+
+function trackMouse(event) {
+  document.documentElement.style.setProperty(
+    '--cursorXPos',
+    `${event.clientX}px`
+  )
+  document.documentElement.style.setProperty(
+    '--cursorYPos',
+    `${event.clientY}px`
+  )
+}
+
+document.addEventListener('mousemove', trackMouse)
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+    <div class="relative z-10 max-w-2xl mx-auto p-4 sm:p-6 lg:p-8 bg-gray-900 text-black">
         <form method="POST" action="{{ route('posts.store') }}">
             @csrf
             <textarea
@@ -19,7 +19,7 @@
                         <path stroke-linecap="round" stroke-linejoin="round" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
                     </svg>
                     <div class="flex-1">
-                        <div class="flex justify-between items-center">
+                        <div class="flex justify-between items-start">
                             <div>
                                 <span class="text-gray-800">{{ $post->user->name }}</span>
                                 <small class="ml-2 text-sm text-gray-600">{{ $post->created_at->format('j M Y, g:i a') }}</small>
@@ -27,7 +27,7 @@
                                     <small class="text-sm text-gray-600"> &middot; {{ __('edited') }}</small>
                                 @endunless
                             </div>
-                            <div class="align block">
+                            <div class="flex items-center ml-2">
                                 @if ($post->user->is(auth()->user()))
                                     <button onclick="window.location='{{route('posts.edit', $post)}}'">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
@@ -37,7 +37,7 @@
                                     <form method="POST" action="{{ route('posts.destroy', $post) }}">
                                             @csrf
                                             @method('delete')
-                                            <button :href="route('posts.destroy', $post)" onclick="event.preventDefault(); this.closest('form').submit();">
+                                            <button class="ml-2" :href="route('posts.destroy', $post)" onclick="event.preventDefault(); this.closest('form').submit();">
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                                                         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
                                                         <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
@@ -54,4 +54,5 @@
         </div>
 
     </div>
+    <div id="torch" class="fixed inset-0 z-20 pointer-events-none"></div>
 </x-app-layout>
